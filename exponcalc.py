ee387edcc670573734c622333246ad0756766714
BuildTools <oscarjmarks@gmail.com>
2016-02-14 07:52:51 +1100

    Added by Oscar

---
+++ exponcalc.py
@@ -0,0 +1,15 @@
+	#An exponents calculator developed by Pixelshower89
+
+print('Hello, and welcome to the Exponents calculator!  X will be raised to the power of Y')
+raw_x = raw_input("What will X be? ")
+raw_y = raw_input('What will Y be? ')
+if raw_y.isdigit()==True and raw_x.isdigit()==True:
+	X=int(raw_x)
+	Y=int(raw_y)
+	result=X**Y
+	print(result)
+	print("Thank you for using Pixelshower89's Exponents calculator")
+else:
+	print("Using letters will crash the program.  Please us numbers in the future.")
+	print("thank you for using Pixelshower89's Exponents calculator")
+
